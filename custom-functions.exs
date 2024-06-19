35af674a9e6f1320dd02edfe736b484ebe4847c8
Manolotsoa Daniel Razafindrakoto <manolotsoadaniel@gmail.com>
2024-06-19 12:27:04 +0300

    function and method

---
+++ custom-functions.exs
@@ -0,0 +1,39 @@
+defmodule Greeter do
+  def hello(name) do
+    "Hello, " <> name
+  end
+end
+
+defmodule GreeterOneLine do
+  def customHello(name), do: "Custom Hello, " <> name
+end
+
+greetings = Greeter.hello("John")
+IO.inspect(greetings)
+
+customGreeting = GreeterOneLine.customHello("John")
+IO.inspect(customGreeting)
+
+defmodule CustomGreeter do
+  def hello() do
+    "Hello anonymous person"
+  end
+
+  def hello(name) do
+    "Hello " <> name
+  end
+
+  def hello(first, second) do
+    "Hello " <> first <> " and" <> second
+  end
+end
+
+anonymousGreetings = CustomGreeter.hello()
+onePersonGreetings = CustomGreeter.hello("John")
+twoPersonGreetings = CustomGreeter.hello("Johnny", "Peter")
+IO.puts("\nanonymous function")
+IO.inspect(anonymousGreetings)
+IO.puts("\none Person function")
+IO.inspect(onePersonGreetings)
+IO.puts("\ntwo Person function")
+IO.inspect(twoPersonGreetings)
